a585c59853d50839f11ea500372759c19babbf99
Rahil Vithalani <rvithalani.rjv@gmail.com>
2024-07-20 19:19:45 +0530

    user model changes

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db.models import DO_NOTHING
 from django.utils.translation import gettext_lazy as _
 
 
@@ -29,23 +30,28 @@ class Role(models.Model):
         The Role entries are managed by the system,
         automatically created via a Django data migration.
     """
-    CLIENT = 1
+    OWNER = 1
     MANAGER = 2
-    OWNER = 3
-    QC = 4
-    ADMIN = 5
+    TEAM_LEAD = 3
+    EMPLOYEE = 4
+    QC_EMPLOYEE = 5
+    CLIENT_MANAGER = 6
+    CLIENT_EMPLOYEE = 7
     ROLE_CHOICES = (
-        (CLIENT, 'client'),
-        (MANAGER, 'manager'),
         (OWNER, 'owner'),
-        (QC, 'qc'),
-        (ADMIN, 'admin'),
+        (MANAGER, 'manager'),
+        (TEAM_LEAD, 'team_lead'),
+        (EMPLOYEE, 'employee'),
+        (QC_EMPLOYEE, 'qc_employee'),
+        (CLIENT_MANAGER, 'client_manager'),
+        (CLIENT_EMPLOYEE, 'client_employee')
     )
 
     id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
 
-    def __str__(self):
-        return self.get_id_display()
+    # TODO: check if we want to render it for frontend directly from here (Reference: get_foo_display())
+    # def __str__(self):
+    #     return self.get_id_display()
 
 
 # Create your models here.
@@ -55,7 +61,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
-    roles = models.ManyToManyField(Role)
+    roles = models.ForeignKey(Role, on_delete=DO_NOTHING, blank=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
